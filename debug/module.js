c7e1204ceb5a839b403ad2d47e2a2c1ffcc0b9b1
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-04-23 16:25:08 +1000

    fix: avoid stripping node: for request of type symbol
    Avoid cases where request = Symbol(esm‍@3.3.5:shared) causing startsWith
    to be undefined, which indirectly cause the "__global__ is not defined"
    error in https://github.com/nktnet1/esm-sync/issues/5

--- debug/module.js
+++ debug/module.js
@@ -1,7 +1,11 @@
 /* eslint-disable node/no-unsupported-features */
+
+import fs from "node:fs"
+
 export const randomVariable = "helloworld"
 
 export {
   randomVariable as default,
-  randomVariable as "module.exports"
+  randomVariable as "module.exports",
+  fs
 }
